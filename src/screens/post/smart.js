db8de48bb61a23bc15ae122f58217487a46e552a
zulkifalkhan <69888921+zulkifalkhan@users.noreply.github.com>
2021-12-22 01:15:08 +0500

    smart serach
    convert location to json

--- src/screens/post/smart.js
+++ src/screens/post/smart.js
@@ -5,10 +5,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
 const { width, height } = Dimensions.get('screen');
 import faker from 'faker'
+import { auth } from "../../../firebase";
+import {db} from "../../../firebase-config"
+import {collection,doc,documentId,getDocs,addDoc,setDoc,updateDoc,query,deleteDoc,where, Firestore} from "firebase/firestore"
+import { createIconSetFromFontello } from "@expo/vector-icons";
 
 faker.seed(5);
 
 
+function getUnique(array){
+    var uniqueArray = [];
+    
+    // Loop through array values
+    for(var i=0; i < array.length; i++){
+        if(uniqueArray.indexOf(array[i]) === -1) {
+            uniqueArray.push(array[i]);
+        }
+    }
+    return uniqueArray;
+  }
+
+
+  function arrayToObject(arr) {
+    var obj = {};
+    for (var i = 0; i < arr.length; ++i){
+        obj[i] = arr[i];
+    }
+    return obj;
+}
 
 
 const DATA = [...Array(10).keys()].map((_, i) => {
@@ -31,6 +55,49 @@ const AVATAR_SIZE = 50;
 
 
 export default function Smart({route,navigation}){
+
+
+    const postCollectionRef = collection(db,"post");
+    const [location,setLocation] = useState([])
+    const [user,setUser] = useState([])
+
+    useEffect(()=>{
+
+        const getPost = async () => {
+            // var id;
+            const data = await getDocs(postCollectionRef);
+            // console.log(data1.data())
+            setUser(data.docs.map((doc) => (
+              {
+                ...doc.data(),
+                id: doc.id
+              })))
+
+              for (var i =0 ;i<user.length;i++) {
+                location.push(user[i].location)
+               }
+      
+          }
+      
+          //console.log(user[0].id)
+
+        var uniqueLocation = getUnique(location);
+        console.log(uniqueLocation);
+
+       var locationNew=arrayToObject(uniqueLocation);
+
+        setLocation(locationNew)
+      
+          getPost()
+      
+        }, [])
+
+
+        // for (var i =0 ;i<location.length;i++){
+        //     console.log(location[i])
+        // }
+
+
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
       <FlatList
@@ -52,7 +119,7 @@ export default function Smart({route,navigation}){
             
             <View style={{marginLeft:60,height:100}}>
               <Text></Text>
-              <Text style={{ fontSize: 22, opacity: .7 }}>Location :{" "}{item.location}</Text>
+              <Text style={{ fontSize: 22, opacity: .7 }}>Location :{" "}</Text>
               <View style={{ flexDirection: 'row' }}>
               <Text style={{marginTop:25,fontSize:18}}>Posts : 5</Text>
                 <TouchableOpacity style={styles.loginBtn1}>
