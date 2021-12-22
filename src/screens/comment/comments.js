8864e0f173587fc370e2c39d431e83370ea1762c
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-22 21:38:52 +0500

    Flat list for comments is created

--- src/screens/comment/comments.js
+++ src/screens/comment/comments.js
@@ -8,30 +8,8 @@ import faker from 'faker'
 import { db } from "../../../firebase-config";
 import { collection, doc, documentId, getDocs, addDoc, setDoc, updateDoc, query, deleteDoc, where, orderBy } from "firebase/firestore"
 
-
-faker.seed(10);
-
-
-
-
-const DATA = [...Array(30).keys()].map((_, i) => {
-    return {
-        key: faker.random.uuid(),
-        image: `https://randomuser.me/api/portraits/${faker.helpers.randomize(['women', 'men'])}/${faker.random.number(60)}.jpg`,
-        name: faker.name.findName(),
-        jobTitle: faker.name.jobTitle(),
-        email: faker.internet.email(),
-        location: 'Muree',
-        budget: "10000",
-        days: "3",
-        status: 'individual',
-        uid: "saqib@test.com"
-    };
-});
-
 const SPACING = 20;
 const AVATAR_SIZE = 50;
-
 
 export default function CommentScreen({ route, navigation }) {
 
@@ -49,9 +27,8 @@ export default function CommentScreen({ route, navigation }) {
 
     useEffect(() => {
         const getPost = async () => {
-            // var id;
-            const q = query(postCollectionRef);
-            // console.log(post[0])
+
+            const q = query(postCollectionRef);      
             const getResult = await getDocs(q)
             getResult.forEach((doc) => {
                 if (doc.id == postId) {
@@ -60,10 +37,12 @@ export default function CommentScreen({ route, navigation }) {
                     setLocation(doc.data().location)
                     setDays(doc.data().days)
                     setDesc(doc.data().description)
-                    //if(doc.data().comments.length > 0) {
-                    setComments(doc.data().comments)
-                    //}
                     setUid(doc.data().uid)
+                    let intermediateList=[]
+                    doc.data().comments.map((item,index)=>{
+                        intermediateList.push({"data":item,"id":index})
+                    })
+                    setComments(intermediateList); 
                 }
             });
 
@@ -74,16 +53,6 @@ export default function CommentScreen({ route, navigation }) {
         getPost()
 
     }, [])
-
-    //   user.sort(function (x, y) {
-    //     return y.postTime - x.postTime;
-    //   })
-
-
-
-    //   function comment(id){
-    //     console.log("Post Id:"+id);
-    //   }
 
 
     function ViewP(id) {
@@ -96,16 +65,10 @@ export default function CommentScreen({ route, navigation }) {
 
     const comFtn = async () => {
 
-        
-        // prevComment.push({ "comment_text": com, "uid": uid })
-        // console.log(comments)
-        // console.log(com)
-        // console.log(uid)
-         console.log(postId)
+        console.log(postId)
         // console.log(prevComment)
         const UpdateComment = async () => {
-            // console.log("testo")
-            // comments.shift()
+            
             let prevComment = comments
             prevComment.push({ "comment_text": com, "uid": uid })
             await updateDoc(doc(db, "post", postId), {
@@ -118,34 +81,63 @@ export default function CommentScreen({ route, navigation }) {
 
     return (
         <View style={{ flex: 1, backgroundColor: '#fff' }}>
-            <View style={{borderWidth:1,borderRadius:10,marginTop:30,marginBottom:40,marginLeft:30,width:"80%",height:"28%",borderColor:"#F08080"}}>
+            <View style={{ borderWidth: 1, borderRadius: 10, marginTop: 30, marginBottom: 40, marginLeft: 30, width: "80%", height: "28%", borderColor: "#F08080" }}>
                 <TouchableOpacity onPress={(() => ViewP(item.uid))}>
-                    <Text style={{ fontSize: 22, fontWeight: '600',marginLeft:100,marginBottom:5 }}>{uid}</Text>
+                    <Text style={{ fontSize: 22, fontWeight: '600', marginLeft: 100, marginBottom: 5 }}>{uid}</Text>
                 </TouchableOpacity>
 
-                <Text style={{ fontSize: 18, opacity: .7,marginLeft:25,marginBottom:5 }}>Going to :{" "}{location}</Text>
-                <Text style={{ fontSize: 18, opacity: .7,marginLeft:25,marginBottom:5 }}>Trip Budget:{" "}{budget}</Text>
-                <Text style={{ fontSize: 18, opacity: .7,marginLeft:25,marginBottom:5 }}>Trip Days:{" "}{days}</Text>
-                <Text style={{ fontSize: 20, opacity: .8,marginLeft:25, color: '#DC143C' }}>{desc}</Text>
+                <Text style={{ fontSize: 18, opacity: .7, marginLeft: 25, marginBottom: 5 }}>Going to :{" "}{location}</Text>
+                <Text style={{ fontSize: 18, opacity: .7, marginLeft: 25, marginBottom: 5 }}>Trip Budget:{" "}{budget}</Text>
+                <Text style={{ fontSize: 18, opacity: .7, marginLeft: 25, marginBottom: 5 }}>Trip Days:{" "}{days}</Text>
+                <Text style={{ fontSize: 20, opacity: .8, marginLeft: 25, color: '#DC143C' }}>{desc}</Text>
             </View>
-            <Text style={{marginLeft:120,marginTop:-20,fontSize:20,color:"#DC143C"}}>Comments:</Text>
-            <View style={{marginTop:10,marginBottom:20}}>  
+            <Text style={{ marginLeft: 120, marginTop: -20, fontSize: 20, color: "#DC143C" }}>Comments:</Text>
+            {/* <View style={{ marginTop: 10, marginBottom: 20 }}>
                 {comments.map(value =>
                     <View style={styles.loginBtn1}>
-                        <Text style={{fontSize:18,marginLeft:10,marginTop:5}}>{value.uid}{"  "}:</Text>
-                        <Text style={{fontSize:18,marginLeft:10,marginTop:5}}>{value.comment_text}</Text>
+                        <Text style={{ fontSize: 18, marginLeft: 10, marginTop: 5 }}>{value.uid}{"  "}:</Text>
+                        <Text style={{ fontSize: 18, marginLeft: 10, marginTop: 5 }}>{value.comment_text}</Text>
                     </View>
                 )}
-            </View>
+            </View> */}
 
-     <TextInput style={{ width:"70%",height: 80,marginLeft: 50,borderWidth: 2,padding: 10,borderColor:'#DC143C',borderRadius:10}}
-        placeholder="Type Comment"
-        onChangeText={value => setCom(value)}
-      />
-                       
-       <TouchableOpacity style={styles.loginBtn} onPress={comFtn}>
-         <Text>Submit Comment</Text>
-       </TouchableOpacity>
+            <FlatList
+                data={comments}
+                keyExtractor={item => item.id}
+                contentContainerStyle={{
+                    padding: SPACING
+                }}
+
+
+                renderItem={({ item, index }) => {
+                    return <View style={{
+                        flexDirection: 'row', padding: SPACING, marginBottom: SPACING, backgroundColor: 'lightgrey',
+                        borderRadius: 16, shadowColor: 'lightgrey', shadowOffset: {
+                            width: 0, height: 10
+                        },
+                        shadowOpacity: .5, shadowRadius: 20
+                    }}>
+
+                        <View>
+                            
+                            <Text style={{ fontSize: 18, opacity: .7 }}>Going to :{" "}{item.data.uid}</Text>
+                            <Text style={{ fontSize: 18, opacity: .7 }}>Trip Budget:{" "}{item.data.comment_text}</Text>
+                            
+                        </View>
+
+
+                    </View>
+                }}
+            />
+
+            <TextInput style={{ width: "70%", height: 80, marginLeft: 50, borderWidth: 2, padding: 10, borderColor: '#DC143C', borderRadius: 10 }}
+                placeholder="Type Comment"
+                onChangeText={value => setCom(value)}
+            />
+
+            <TouchableOpacity style={styles.loginBtn} onPress={comFtn}>
+                <Text>Submit Comment</Text>
+            </TouchableOpacity>
 
         </View>
     );
@@ -179,10 +171,10 @@ const styles = StyleSheet.create({
         height: 40,
         alignItems: "center",
         justifyContent: "center",
-        marginLeft:100,
+        marginLeft: 100,
         marginTop: 20,
         backgroundColor: "#F08080",
-        flexDirection:"row"
+        flexDirection: "row"
 
     },
     loginBtn1: {
@@ -191,9 +183,9 @@ const styles = StyleSheet.create({
         width: "90%",
         borderRadius: 15,
         height: 70,
-        marginLeft:10,
+        marginLeft: 10,
         marginTop: 10,
-        flexDirection:"row",
+        flexDirection: "row",
         backgroundColor: "#F08080",
     }
 });
