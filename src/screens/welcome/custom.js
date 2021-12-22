7dd65504d9865090ad1b5e27d0ce22628ae53078
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-23 02:35:40 +0500

    Update custom.js

--- src/screens/welcome/custom.js
+++ src/screens/welcome/custom.js
@@ -64,12 +64,12 @@ export default function CustomSide({route,navigation}) {
         </View>
       </TouchableOpacity>
      
-      {/* <TouchableOpacity onPress={() => navigation.navigate('display_group')}>
+      <TouchableOpacity onPress={() => navigation.navigate('display_group')}>
       <View style={{flexDirection:'row',marginTop:20}}>
       <MaterialIcons name="groups" size={24} color="black" style={{marginTop:10,paddingLeft:10}} />
         <Text style={{color:'white',fontSize:16,marginTop:10,paddingLeft:10}}>Groups</Text>
         </View>
-        </TouchableOpacity> */}
+        </TouchableOpacity>
 
         <TouchableOpacity onPress={() => navigation.navigate('display_invite')}>
       <View style={{flexDirection:'row',marginTop:20}}>
