9af59c76e4eee41e84d61c545297f38911c3cd27
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-24 05:51:00 +0500

    Update signIn.js

--- src/screens/login/signIn.js
+++ src/screens/login/signIn.js
@@ -1,38 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator,Header } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, Header } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input,Button, } from 'react-native-elements';
+import { Input, Button, } from 'react-native-elements';
 import {
   View,
-   TouchableOpacity, Animated, Text, Image, FlatList, StyleSheet, Dimensions, KeyboardAvoidingView,ScrollView
+  TouchableOpacity, Animated, Text, Image, FlatList, StyleSheet, Dimensions, KeyboardAvoidingView, ScrollView
 } from "react-native";
 import { auth } from "../../../firebase";
+import { useIsFocused } from "@react-navigation/native";
 
-
-var keyboardH=0
+var keyboardH = 0
 
 if (Platform.OS === 'ios') {
   keyboardH = 1
 } else {
-  keyboardH= -400
+  keyboardH = -400
 }
 
 
 export default function SignIn({ navigation }) {
-
+  const isFocused = useIsFocused(); //for update of state variables
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [status, setStatus] = useState("");
   const [uid, setUserId] = useState("");
 
-  const nav =()=>{
+  const nav = () => {
     console.log(uid)
-    const unsubscribe = auth.onAuthStateChanged(user=>{
-      if(user){
-        navigation.navigate('drawer',{
-          userId:email
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.navigate('drawer', {
+          userId: email
         })
       }
     })
@@ -42,79 +42,92 @@ export default function SignIn({ navigation }) {
 
   const handleSignIn = () => {
     auth
-    .signInWithEmailAndPassword(email,password)
-    .then(userCredentials =>{
-      const user = userCredentials.user;
-      //console.log('Logged in with : '+user.email)
-      setUserId(user.email)
-    //  console.log(uid)
-      nav()
-    })
-    .catch(error => alert(error.message))
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        //console.log('Logged in with : '+user.email)
+        setUserId(user.email)
+        //  console.log(uid)
+        nav()
+      })
+      .catch(error => alert(error.message))
 
-}
-
-
-
-return (
-      <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset = {keyboardH}> 
-    <ScrollView style={{backgroundColor:'white'}}>
-  <View>
-    <View style={{
-      backgroundColor:'#F08080',
-      position:'absolute',
-      width:400,
-      height:400,
-      borderRadius:200,
-      right:-100,
-      top:-200,
-    }}></View>
-    <View style={{
-      backgroundColor:'#FFB6C1',
-      position:'absolute',
-      width:200,
-      height:200,
-      borderRadius:100,
-      left:-50,
-      top:-50,
-    }}></View>
-
-    <View style={{paddingVertical:220,justifyContent:'center',alignItems:'center'}}>
-    <Text style={{fontSize:30,color:'grey',marginBottom:60}}>Welcome to Travel Mate</Text>
-
-<Input
-  placeholder='Enter your Email' 
-  leftIcon={
-    <Icon
-      name='user'
-      size={24}
-      color='black'
-    />
   }
-  onChangeText={value=> setEmail(value)}
-/>  
-<Input placeholder="Password"
-  leftIcon={
-    <Icon
-      name='lock'
-      size={24}
-      color='black'
-    />
-  }
-onChangeText={value=> setPassword(value)}
-secureTextEntry={true} />
+
+  useEffect(() => {
+    setEmail("")
+    setPassword("")
+    setUserId("")
+    if (isFocused) {
+      setEmail("")
+      setPassword("")
+      setUserId("")
+    }
+  }, [isFocused])
 
 
-       <TouchableOpacity style={styles.loginBtn} onPress={handleSignIn}>
-         <Text>Sign In</Text>
-       </TouchableOpacity>
 
-    </View>
+  return (
+    <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={keyboardH}>
+      <ScrollView style={{ backgroundColor: 'white' }}>
+        <View>
+          <View style={{
+            backgroundColor: '#F08080',
+            position: 'absolute',
+            width: 400,
+            height: 400,
+            borderRadius: 200,
+            right: -100,
+            top: -200,
+          }}></View>
+          <View style={{
+            backgroundColor: '#FFB6C1',
+            position: 'absolute',
+            width: 200,
+            height: 200,
+            borderRadius: 100,
+            left: -50,
+            top: -50,
+          }}></View>
 
-   </View>
-   </ScrollView>
-   </KeyboardAvoidingView>
-)
+          <View style={{ paddingVertical: 220, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 30, color: 'grey', marginBottom: 60 }}>Welcome to Travel Mate</Text>
+
+            <Input
+              placeholder='Enter your Email'
+              leftIcon={
+                <Icon
+                  name='user'
+                  size={24}
+                  color='black'
+                />
+              }
+              value={email}
+              onChangeText={value => setEmail(value)}
+            />
+            <Input placeholder="Password"
+              leftIcon={
+                <Icon
+                  name='lock'
+                  size={24}
+                  color='black'
+                />
+              }
+              value={password}
+              onChangeText={value => setPassword(value)}
+              secureTextEntry={true} />
+
+
+            <TouchableOpacity style={styles.loginBtn} onPress={handleSignIn}>
+              <Text>Sign In</Text>
+            </TouchableOpacity>
+
+          </View>
+
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  )
 }
 
 
@@ -126,10 +139,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   loginBtn: {
-    borderWidth:1,
-    borderColor:'rgba(0,0,0.2,0.2)',
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0.2,0.2)',
     width: "40%",
-    borderRadius:45,
+    borderRadius: 45,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
