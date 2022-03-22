0b52dc3d6120ca91fa3bced0b13dba7540c5c35d
Nabeel Noor <nabeelnoor914@gmail.com>
2022-03-23 04:25:15 +0500

    removing minor bugs

--- src/screens/signin.js
+++ src/screens/signin.js
@@ -3,16 +3,16 @@ import { View, Text, SafeAreaView,StyleSheet } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 
 import Post from "./Post";  //Post component
-import {POSTS} from "../data/posts"  //data component
+import { POSTS } from '../data/posts'; //Post data
 
 
 const signin = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        {POSTS.map((post,index)=> (
-            <Post post={post} key={index}></Post>
-        ))}
+       {POSTS.map((element,index)=>(
+         <Post post={element} key={index}></Post>
+       ))} 
       </ScrollView>
     </SafeAreaView>
   )
