bad4ea80c69550a6c0f6ea0c2d0820cd6f426843
Nabeel Noor <nabeelnoor914@gmail.com>
2022-03-29 17:10:58 +0500

    making conditions based on bits for string

--- src/UtilPackages/String.js
+++ src/UtilPackages/String.js
@@ -1,48 +1,69 @@
-// export const getTokenizeContent = (sampleString) => {
-//     try {
+const getQuoteCheck=(flag,sampleString)=>{
+    let firstFlag=false;
+    let lastFlag=false;
 
-//         let Qflag = false; //Quotation check
-//         let stringArr = sampleString.split(' ') //convert string to token basis on spaces
-//         let formattedStringArr = stringArr.map(elem => { //make formatted string with indicator
-//             if (elem[0] == "#") {
-//                 return [1, elem]
-//             } else if (elem.includes("\"") || Qflag == true) {
-//                 if (elem.includes("\"")) {
-//                     if (Qflag == true) Qflag = false;
-//                     else Qflag = true;
-//                 }
-//                 return [2, elem]
-//             } else {
-//                 return [0, elem]
-//             }
-//         })
-//         return formattedStringArr
-//     }
-//     catch (e) {
-//         return []
-//     }
-// }
+    if(sampleString[0]=="\""){
+        firstFlag=true;
+    }
+    if(sampleString[sampleString.length-1]=="\""){
+        lastFlag=true;
+    }
+
+    //base case : 000 and 100
+    if(firstFlag==false && lastFlag==false){
+        if(flag){
+            return [flag,[[2,sampleString,1]]]
+        }else{
+            return [flag,[[0,sampleString,1]]]
+        }
+    }
+
+    /* 001 010 011 101 110 111 */
+    if(flag==false && firstFlag==true && lastFlag==true){ //011
+        return [false,[[2,sampleString,1]]]
+    }
+    if(flag==false && firstFlag==true && lastFlag==false){ //010
+        return [true,[[2,sampleString,1]]]
+    }
+    if(flag==false && firstFlag==false && lastFlag==true){//001
+        return [true,[[0,sampleString,1]]]
+    }
+
+    if(flag==true && firstFlag==true && lastFlag==true){ //!11
+        return [true,[[0,sampleString,1]]]
+    }
+    if(flag==true && firstFlag==true && lastFlag==false){ //110
+        return [false,[[0,sampleString,1]]]
+    }
+    if(flag==true && firstFlag==false && lastFlag==true){//101
+        return [false,[[2,sampleString,1]]]
+    }
+
+}
 
 const getHashQuoteToken=(lineQuoteFlag,sampleString)=>{
     //func to return quoted and hash format
     try{
         let Qflag = lineQuoteFlag; //Quotation check
         let stringArr = sampleString.split(' ') //convert string to token basis on spaces
-        
+        let mainReturnArr=[]
         let formattedStringArr = stringArr.map(elem => { //make formatted string with indicator
             if (elem[0] == "#") {
-                return [1, elem]
+                mainReturnArr.push([1, elem,1])
             } else if (elem.includes("\"") || Qflag == true) {
                 if (elem.includes("\"")) {
-                    if (Qflag == true) Qflag = false;
-                    else Qflag = true;
-                }
-                return [2, elem]
+                    let retArr=getQuoteCheck(Qflag,elem)
+                    Qflag=retArr[0]
+                    retArr=retArr[1]
+                    retArr.map(x=>mainReturnArr.push(x))
+                    // if (Qflag == true) Qflag = false;
+                    // else Qflag = true;
+                }else { mainReturnArr.push([2, elem,1]) }
             } else {
-                return [0, elem]
+                mainReturnArr.push([0, elem,1])
             }
         })
-        return [Qflag,formattedStringArr]
+        return [Qflag,mainReturnArr]
     }
     catch(e){
         return []
